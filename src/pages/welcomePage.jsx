30dc6da751600029f10efabf2cf90ba0a1c497da
Elonmusk <srujanaj313@gmail.com>
2025-06-03 09:21:35 +0530

    removed unwanted files

--- src/pages/welcomePage.jsx
+++ src/pages/welcomePage.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect} from 'react'
 import MainLayout from "./MainLayout";
+import { useSnackbar } from "../context/SnackbarContext";
 
 function WelcomePage() {
+  const showSnackbar = useSnackbar();
+  useEffect(()=> {
+    showSnackbar("login Successful!!!.", 5000);
+  },[])
   return (
     <>
     <MainLayout />
